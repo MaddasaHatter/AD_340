50aec79bf23b86ab5e827c4738bd49737d51e9a0
Lilith Lucifer <lilith.caldwell@seattlecolleges.edu>
2023-05-02 19:25:43 -0700

    name changes

--- Homework/Week_3/click_counter/app/src/main/java/com/example/click_counter/MainActivity.kt
+++ Homework/Week_3/click_counter/app/src/main/java/com/example/click_counter/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.click_counter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import com.example.click_counter.R
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
