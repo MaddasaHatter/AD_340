64a74cfda2ebcc217b8c059ceb047b5c761ec5eb
Lilith Lucifer <lilith.caldwell@seattlecolleges.edu>
2023-05-06 17:04:09 -0700

    new files

--- Homework/Week_4/binding_toast/app/src/main/java/com/example/bindingtoast/MainActivity.kt
+++ Homework/Week_4/binding_toast/app/src/main/java/com/example/bindingtoast/MainActivity.kt
@@ -2,23 +2,22 @@ package com.example.bindingtoast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.bindingtoast.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-        binding.textview01.setOnClickListener {
-            binding.textview01.text = "goodbye"
-            
-            val button = findViewById<Button>(R.id.button)
-            button.setOnClickListener {
-                val text = "Don't Touch Me!"
+
+            binding.button.setOnClickListener {
+                binding.name = "Don't Touch Me, Minion!"
                 val duration = Toast.LENGTH_SHORT
-                val toast = Toast.makeText(this, text, duration)
+                val toast = Toast.makeText(applicationContext, binding.name, duration)
                 toast.show()
     }
+}
 }
